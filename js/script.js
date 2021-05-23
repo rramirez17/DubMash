43f760f7a48cad0e7081f247f1f1252d241b23c5
Raúl Ramírez <raul.ramirez@utp.ac.pa>
2021-05-23 09:39:57 -0500

    Se agregó la parte funcional a la barra de búsqueda y margenes al área central

--- js/script.js
+++ js/script.js
@@ -4,11 +4,25 @@ const closeBtn = document.getElementsByClassName('close')[0]
 
 for (let i = 0; i < images.length; i++) {
     images[i].onclick = () =>{
+        titulopost=document.getElementById("BusquedaTitle").textContent="Título del Post"
         overlay.classList.toggle('hidden')
         document.getElementsByTagName('body')[0].classList.toggle('lockScroll')
     }
     
 }
+closeBtn.onclick = () =>{
+    overlay.classList.toggle
+    etElementsByTagName('body')[0].classList.toggle('lockScroll')
+}
+
+
+query1.onclick =() =>{
+    titulopost=document.getElementById("BusquedaTitle").textContent=document.getElementById('textobusqueda').value
+    overlay.classList.toggle('hidden')
+    document.getElementsByTagName('body')[0].classList.toggle('lockScroll')
+}   
+
+
 closeBtn.onclick = () =>{
     overlay.classList.toggle('hidden')
     document.getElementsByTagName('body')[0].classList.toggle('lockScroll')
